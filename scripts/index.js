7d506a83fdd85c0af07802bb8a64e7030191e5e9
Nikolay Petrovskiy <n.kzn@yandex.ru>
2022-01-15 21:34:49 +0300

    feat: Добавить popup

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,19 @@
 let content = document.querySelector('.content');
-let formElement = document.querySelector('.popup__form');
 let editProfileInfoButton = content.querySelector('.profile__edit-button');
-let closeProfileInfoButton = content.querySelector('.popup__button-close');
-let popupOpen = content.querySelector('.popup');
+let page = document.querySelector('.page');
+let formElement = page.querySelector('.popup__container');
+let closeProfileInfoButton = page.querySelector('.popup__button-close');
+let popupOpen = page.querySelector('.popup_type_profile');
 
 
 function formSubmitHandler (evt) {
     evt.preventDefault(); 
-    let userName = document.querySelector('.popup__input-name').value;
-    let userPosition = document.querySelector('.popup__input-position').value;
-    let profileName = document.querySelector('.profile__profile-name');
-    let profilePosition = document.querySelector('.profile__profile-position');
+    let userName = page.querySelector('.popup__input-name').value;
+    let userJob = page.querySelector('.popup__input-job').value;
+    let profileName = document.querySelector('.profile__name');
+    let profileJob = document.querySelector('.profile__job');
     profileName.textContent = userName;
-    profilePosition.textContent = userPosition;
+    profileJob.textContent = userJob;
     popupOpen.classList.remove('popup_opened'); 
 }
 
@@ -27,32 +28,3 @@ editProfileInfoButton.addEventListener('click', function(evt) {
     evt.preventDefault();
     popupOpen.classList.add('popup_opened');
 });
-
-
-
-
-/*
-// Находим форму в DOM
-let formElement = // Воспользуйтесь методом querySelector()
-// Находим поля формы в DOM
-let nameInput = // Воспользуйтесь инструментом .querySelector()
-let jobInput = // Воспользуйтесь инструментом .querySelector()
-
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
-function formSubmitHandler (evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-                                                // Так мы можем определить свою логику отправки.
-                                                // О том, как это делать, расскажем позже.
-
-    // Получите значение полей jobInput и nameInput из свойства value
-
-    // Выберите элементы, куда должны быть вставлены значения полей
-
-    // Вставьте новые значения с помощью textContent
-}
-
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
-formElement.addEventListener('submit', formSubmitHandler);
-*/
