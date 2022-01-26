352e645a3c0daa1989b39dea7edbedc3f8b2a6d1
Nikolay Petrovskiy <n.kzn@yandex.ru>
2022-01-26 15:41:31 +0300

    feat: Добавить popup mesto

--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,50 @@
-let content = document.querySelector('.content');
-let editProfileInfoButton = content.querySelector('.profile__edit-button');
-let page = document.querySelector('.page');
-let formElement = page.querySelector('.popup__container');
-let closeProfileInfoButton = page.querySelector('.popup__button-close');
-let popupOpen = page.querySelector('.popup_type_profile');
-let userName = page.querySelector('.popup__input_field_name');
-let userJob = page.querySelector('.popup__input_field_job');
-let profileName = document.querySelector('.profile__name');
-let profileJob = document.querySelector('.profile__job');
+const content = document.querySelector('.content');
+const page = document.querySelector('.page');
+const formElement = page.querySelector('.popup__container');
+const popupProfile = page.querySelector('.popup_type_profile');
+const popupMesto = page.querySelector('.popup_type_mesto');
+const editProfileInfoButton = page.querySelector('.profile__edit-button');
+const closeProfileInfoButton = popupProfile.querySelector('.popup__button-close');
+const addMestoButton = page.querySelector('.profile__add-button');
+const popupCloseMesto = popupMesto.querySelector('.popup__button-close');
+const userName = popupProfile.querySelector('.popup__input_field_name');
+const userJob = popupProfile.querySelector('.popup__input_field_job');
+const profileName = page.querySelector('.profile__name');
+const profileJob = page.querySelector('.profile__job');
 
-function formSubmitHandler (evt) {
-    evt.preventDefault(); 
+
+function formSubmitHandlerProfile (evt) {
+    evt.preventDefault();
     profileName.textContent = userName.value;
     profileJob.textContent = userJob.value;
-    closePopup () 
+    close(popupProfile);
 }
 
-function openPopup () {
-    popupOpen.classList.add('popup_opened');
+function formSubmitHandlerMesto (evt) {
+    evt.preventDefault(); 
+    //profileName.textContent = userName.value;
+    //profileJob.textContent = userJob.value;
+    close(popupMesto);
+}
+
+function openPopupProfile() {
+    open(popupProfile);
     userName.value = profileName.textContent;
     userJob.value = profileJob.textContent;
 }
 
-function closePopup () {
-    popupOpen.classList.remove('popup_opened'); 
+function open(popup) {
+    popup.classList.add('popup_opened');
 }
 
-editProfileInfoButton.addEventListener('click', openPopup);
+function close(popup) {
+    popup.classList.remove('popup_opened');
+}
 
-closeProfileInfoButton.addEventListener('click', closePopup);
+editProfileInfoButton.addEventListener('click', openPopupProfile);
+closeProfileInfoButton.addEventListener('click', () => close(popupProfile));
+addMestoButton.addEventListener('click', () => open(popupMesto));
+popupCloseMesto.addEventListener('click', () => close(popupMesto));
 
-formElement.addEventListener('submit', formSubmitHandler);
+formElement.addEventListener('submit', formSubmitHandlerProfile);
+formElement.addEventListener('submit', formSubmitHandlerMesto);
