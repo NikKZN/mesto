4cb7b6ddb46e8fc9d77b841a645869a53574df0a
Nikolay Petrovskiy <n.kzn@yandex.ru>
2022-01-28 16:27:33 +0300

    feat: Добавить карточки по клику

--- scripts/index.js
+++ scripts/index.js
@@ -13,6 +13,8 @@ const userName = popupProfile.querySelector('.popup__input_field_name');
 const userJob = popupProfile.querySelector('.popup__input_field_job');
 const profileName = page.querySelector('.profile__name');
 const profileJob = page.querySelector('.profile__job');
+const mestoName = popupMesto.querySelector('.popup__input_field_mesto');
+const mestoLink = popupMesto.querySelector('.popup__input_field_link');
 const initialCards = [
     {
       name: 'Архыз',
@@ -38,7 +40,7 @@ const initialCards = [
       name: 'Байкал',
       link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
     }
-  ];
+];
 
 const cardTemplate = page.querySelector('.template').content; //Находим template
 const elementsList = page.querySelector('.elements__list'); //Находим место вставки
@@ -72,43 +74,44 @@ function renderItem(item) {
     deleteButtonCard.closest('.elements__item').remove();
   });
   //------Добавляем элементы в DOM
-  elementsList.append(newItem);
+  elementsList.prepend(newItem);
 }
-
+//--------Форма отправки данных пользователя
 function formSubmitHandlerProfile (evt) {
     evt.preventDefault();
     profileName.textContent = userName.value;
     profileJob.textContent = userJob.value;
     close(popupProfile);
 }
-
+//--------Форма отправки данных места
 function formSubmitHandlerMesto (evt) {
-    evt.preventDefault(); 
-    //profileName.textContent = userName.value;
-    //profileJob.textContent = userJob.value;
+    evt.preventDefault();
+    renderItem({name: mestoName.value, link: mestoLink.value});
+    mestoName.value = '';
+    mestoLink.value = '';
     close(popupMesto);
 }
-
+//--------Функция открытия попапа профиля
 function openPopupProfile() {
     open(popupProfile);
     userName.value = profileName.textContent;
     userJob.value = profileJob.textContent;
 }
-
+//--------Функция открытия попапа
 function open(popup) {
     popup.classList.add('popup_opened');
 }
-
+//--------Функция закрытия попапа
 function close(popup) {
     popup.classList.remove('popup_opened');
 }
 
-editProfileInfoButton.addEventListener('click', openPopupProfile);
-closeProfileInfoButton.addEventListener('click', () => close(popupProfile));
-addMestoButton.addEventListener('click', () => open(popupMesto));
-popupCloseMesto.addEventListener('click', () => close(popupMesto));
-closePopapImage.addEventListener('click', () => close(popupImage));
+editProfileInfoButton.addEventListener('click', openPopupProfile); //Слушатель открытия попапа профиль
+closeProfileInfoButton.addEventListener('click', () => close(popupProfile)); //Слушатель закрытия попапа профиль
+addMestoButton.addEventListener('click', () => open(popupMesto)); //Слушатель открытия попапа место
+popupCloseMesto.addEventListener('click', () => close(popupMesto)); //Слушатель закрытия попапа место
+closePopapImage.addEventListener('click', () => close(popupImage)); //Слушатель закрытия попапа просмотра изображения карточки
 
-formElement.addEventListener('submit', formSubmitHandlerProfile);
-formElement.addEventListener('submit', formSubmitHandlerMesto);
+popupProfile.addEventListener('submit', formSubmitHandlerProfile); //Слушатель отправки формы профиль
+popupMesto.addEventListener('submit', formSubmitHandlerMesto); //Слушатель отправки формы место
 render();
